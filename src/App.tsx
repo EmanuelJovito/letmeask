35ab44f2d0921b29d10e6a0b0905f2d16fe06112
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-06-24 10:08:31 -0300

    feat: Criando contexto de autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import { createContext, useState } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { firebase, auth } from './services/firebase'
+
 import { NewRoom } from './pages/NewRoom'
-// import { Home } from './pages/Home'
+import { Home } from './pages/Home'
 
-import './services/firebase'
+type User = {
+  id: string
+  name: string
+  avatar: string
+}
 
-import './styles/global.scss'
+type AuthContextType = {
+  user: User | undefined
+  signInWithGoogle: () => void
+}
+
+export const AuthContext = createContext({} as AuthContextType)
 
 export function App(): React.ReactElement {
-  return <NewRoom />
+  const [user, setUser] = useState<User>()
+
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    auth.signInWithPopup(provider).then(result => {
+      if (result.user) {
+        const { displayName, photoURL, uid } = result.user
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account')
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    })
+  }
+
+  return (
+    <BrowserRouter>
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContext.Provider>
+    </BrowserRouter>
+  )
 }
