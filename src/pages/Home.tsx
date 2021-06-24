c2b7f39e6be777a46541c90256a9ec8498217840
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-06-24 13:53:27 -0300

    feat: Criando autenticação e redirecionamento de usuario

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { useHistory } from 'react-router-dom'
+
+import { useAuth } from '../hooks/useAuth'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -7,6 +11,17 @@ import { Button } from '../components/Button'
 import '../styles/auth.scss'
 
 export function Home(): React.ReactElement {
+  const history = useHistory()
+  const { user, signInWithGoogle } = useAuth()
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
+    history.push('/rooms/new')
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -20,7 +35,7 @@ export function Home(): React.ReactElement {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
